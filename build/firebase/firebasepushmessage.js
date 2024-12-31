0abea1086e57dcad807c2d62385eba61f166c111
pravin-on-cloud <pravinraja@clouddesk.ae>
2024-12-31 11:47:35 +0530

    changes

--- build/firebase/firebasepushmessage.js
+++ build/firebase/firebasepushmessage.js
@@ -1,24 +1,25 @@
 import admin from "firebase-admin";
-import { query } from "../database/postgres.js";
-import serviceData from './service.json' assert { type: 'json' };
+// import { query } from "../database/postgres.js";
+// import serviceData from './service.json' assert { type: 'json' };
 let userTokenData;
-admin.initializeApp({
-    credential: admin.credential.cert(serviceData),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceData as admin.ServiceAccount),
+// });
 export const messageinitialization = async (userid, messageData) => {
     try {
-        const queryText = `SELECT * FROM users where id = $1`;
-        const dataresult = await query(queryText, [userid]);
-        if (dataresult.rows.length > 0) {
-            if (dataresult.rows[0].fcmid && dataresult.rows[0].fcmid != null) {
-                userTokenData = dataresult.rows[0].fcmid;
-                const userToken = userTokenData;
-                let data = await sendPushNotification(userToken, messageData);
-            }
-            else {
-                return 'No Push Notification Method Present ';
-            }
-        }
+        // const queryText = `SELECT * FROM users where id = $1`;
+        // const dataresult = await query(queryText, [userid]);
+        // if (dataresult.rows.length > 0) {
+        //   if (dataresult.rows[0].fcmid && dataresult.rows[0].fcmid != null) {
+        //     userTokenData = dataresult.rows[0].fcmid
+        //     const userToken = userTokenData
+        //     let data = await sendPushNotification
+        //       (userToken, messageData);
+        //   }
+        //   else {
+        //     return 'No Push Notification Method Present '
+        //   }
+        // }
     }
     catch (error) {
         return error.message;
